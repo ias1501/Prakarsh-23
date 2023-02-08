f64f984a2e0f0680077f17e01937b08c97147988
VadhwanaParth15 <parthvadhwana050@gmail.com>
2023-02-08 11:30:05 +0530

    hj

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ const App = () => {
         <NavBar/>
         <Home/>
         <About />
-        {/* <Event />
-        <Team /> */}
+        <Event />
+        <Team />
         {/* <Sponsor /> */}
         <Glimpse />
         <Footer />
